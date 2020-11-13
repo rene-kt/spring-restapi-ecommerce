fd850a71cf38dd076719f1fa5f3ea3ae85724dd1
Renê <renejr.arraes286@gmail.com>
2020-11-13 09:13:38 -0300

    create class client

---
+++ src/main/java/com/rene/ecommerce/domain/Client.java
@@ -0,0 +1,56 @@
+package com.rene.ecommerce.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity	
+public class Client implements Serializable {
+	
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+   
+	private String name;
+    private String cpf; 
+    private String email;
+    private String password;
+    
+
+    public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
