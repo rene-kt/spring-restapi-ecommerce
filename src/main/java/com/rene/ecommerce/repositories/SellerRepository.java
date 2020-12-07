4dce49a3372c06b5c0ced311f0ac0b60074b12d6
Renê <renejr.arraes286@gmail.com>
2020-12-07 13:34:42 -0300

    doing the same thing but with seller list

--- src/main/java/com/rene/ecommerce/repositories/SellerRepository.java
+++ src/main/java/com/rene/ecommerce/repositories/SellerRepository.java
@@ -9,21 +9,18 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.rene.ecommerce.domain.dto.ranking.SellerRankingDTO;
 import com.rene.ecommerce.domain.users.Seller;
-
 
 @Repository
 public interface SellerRepository extends JpaRepository<Seller, Integer> {
 
 	@Transactional
 	Seller findByEmail(String email);
-	
+
 	@Transactional
 	Seller findByCpf(String cpf);
-	
 
 	@Modifying
-	@Query(value="select id, name, numberOfSells,   howMuchMoneyThisSellerHasSold from tb_sellers order by howMuchMoneyThisSellerHasSold DESC limit 10 ",nativeQuery = true)
-	List<SellerRankingDTO> returnRankingSeller();
+	@Query(value = "select * from tb_sellers order by how_much_money_this_seller_has_sold DESC limit 10 ", nativeQuery = true)
+	List<Seller> returnRankingSeller();
 }
--- src/main/java/com/rene/ecommerce/services/RankingService.java
+++ src/main/java/com/rene/ecommerce/services/RankingService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.rene.ecommerce.domain.dto.ranking.ClientRankingDTO;
 import com.rene.ecommerce.domain.dto.ranking.SellerRankingDTO;
 import com.rene.ecommerce.domain.users.Client;
+import com.rene.ecommerce.domain.users.Seller;
 import com.rene.ecommerce.repositories.ClientRepository;
 import com.rene.ecommerce.repositories.SellerRepository;
 
@@ -17,27 +18,30 @@ public class RankingService {
 
 	@Autowired
 	private ClientRepository clientRepo;
-	
 
 	@Autowired
 	private SellerRepository sellerRepo;
-	
-	public List<ClientRankingDTO> returnRankingClient(){
-		
+
+	public List<ClientRankingDTO> returnRankingClient() {
+
 		List<Client> clients = clientRepo.returnRankingClient();
 		List<ClientRankingDTO> rankingDTO = new ArrayList<>();
-		
-		clients.stream().forEach( x-> rankingDTO.add(new ClientRankingDTO
-				(x.getId(), x.getName(), x.getNumberOfBuys(), 
-						x.getHowMuchMoneyThisClientHasSpent())));
-		
-		
+
+		clients.stream().forEach(x -> rankingDTO.add(new ClientRankingDTO(x.getId(), x.getName(), x.getNumberOfBuys(),
+				x.getHowMuchMoneyThisClientHasSpent())));
+
 		return rankingDTO;
 	}
-	
-	public List<SellerRankingDTO> returnRankingSeller(){
-		
-		return sellerRepo.returnRankingSeller();
+
+	public List<SellerRankingDTO> returnRankingSeller() {
+
+		List<Seller> sellers = sellerRepo.returnRankingSeller();
+		List<SellerRankingDTO> rankingDTO = new ArrayList<>();
+
+		sellers.stream().forEach(x -> rankingDTO.add(new SellerRankingDTO(x.getId(), x.getName(), x.getNumberOfSells(),
+				x.getHowMuchMoneyThisSellerHasSold())));
+
+		return rankingDTO;
 	}
-	
+
 }
