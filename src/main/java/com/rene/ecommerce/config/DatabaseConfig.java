2adf52cf2964413c0d23718c7d57cd5d26040b1a
yzhang <zyk0330@outlook.com>
2023-04-19 17:51:18 -0400

    added dependencies for testing

--- src/main/java/com/rene/ecommerce/config/DatabaseConfig.java
+++ src/main/java/com/rene/ecommerce/config/DatabaseConfig.java
@@ -8,13 +8,21 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
 
-  @Bean
-  public DataSource dataSource() {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-  }
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
 }
