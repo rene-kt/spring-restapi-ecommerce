dcf8c27c02594691dddaade08e8465323a466946
yzhang <zyk0330@outlook.com>
2023-04-19 21:32:56 -0400

    add test cases for seller resource

--- src/test/java/com/rene/ecommerce/APITest.java
+++ src/test/java/com/rene/ecommerce/APITest.java
@@ -19,16 +19,25 @@ import static org.junit.jupiter.api.Assertions.*;
 public class APITest {
 
     public static String baseUrl = "http://localhost:3000/";
-    private static String token = "";
-    private static String email = "abc@dgmail.com";
+    private static String clientEmail = "client1@gmail.com";
+    private static String clientPasswd = "client1";
+    private static String clientToken = "";
+    private static String sellerEmail = "seller1@gmail.com";
+    private static String sellerPasswd = "seller1";
+    private static String sellerToken = "";
 
     @BeforeAll
     // login and get token
     public static void login() {
-        String response = login(email, "client1");
+        String response = login(clientEmail, clientPasswd);
         assertTrue(response.startsWith("Bearer "));
         assertTrue(response.length() > 7);
-        token = response;
+        clientToken = response;
+
+        response = login(sellerEmail, sellerPasswd);
+        assertTrue(response.startsWith("Bearer "));
+        assertTrue(response.length() > 7);
+        sellerToken = response;
     }
 
     public static String login(String email, String password) {
@@ -60,17 +69,31 @@ public class APITest {
 
     // 1.2 GET /user : return if the user is a client or a seller
     @Test
-    public void testUser() {
+    public void testUserClient() {
         String url = baseUrl + "user";
         Response response = given().
-                header("Authorization", token).
+                header("Authorization", clientToken).
                 when().
                 get(url).
                 then().
                 statusCode(200).
                 extract().response();
         String role = response.jsonPath().getString("type");
-        assertTrue(role.equals("Client") || role.equals("Seller"));
+        assertEquals(role, "Client");
+    }
+
+    @Test
+    public void testUserSeller() {
+        String url = baseUrl + "user";
+        Response response = given().
+                header("Authorization", sellerToken).
+                when().
+                get(url).
+                then().
+                statusCode(200).
+                extract().response();
+        String role = response.jsonPath().getString("type");
+        assertEquals(role, "Seller");
     }
 
     // 2. test ClientResource
@@ -79,14 +102,14 @@ public class APITest {
     public void testClient() {
         String url = baseUrl + "client";
         Response response = given().
-                header("Authorization", token).
+                header("Authorization", clientToken).
                 when().
                 get(url).
                 then().
                 statusCode(200).
                 extract().response();
-        assertTrue(response.jsonPath().getString("email").equals(email));
-        assertTrue(response.jsonPath().getJsonObject("boughtProducts") != null);
+        assertEquals(response.jsonPath().getString("email"), clientEmail);
+        assertEquals(response.jsonPath().getString("type"), "Client");
     }
 
     // 2.2 GET /clients: return all clients
@@ -94,7 +117,7 @@ public class APITest {
     public void testClients() {
         String url = baseUrl + "clients";
         Response response = given().
-                header("Authorization", token).
+                header("Authorization", clientToken).
                 when().
                 get(url).
                 then().
@@ -109,7 +132,7 @@ public class APITest {
     public void testClientRanking() {
         String url = baseUrl + "clients/ranking";
         Response response = given().
-                header("Authorization", token).
+                header("Authorization", clientToken).
                 when().
                 get(url).
                 then().
@@ -158,7 +181,7 @@ public class APITest {
                 post(url).
                 then().
                 extract().response();
-        assertTrue(resp.statusCode() == 400);
+        assertEquals(resp.statusCode(), 400);
         assertTrue(resp.jsonPath().getString("message").
                 contains("This email is already being used"));
     }
@@ -222,6 +245,153 @@ public class APITest {
         login("testCreate1@gmail.com", "testCreate1", 401);
     }
 
-    // 3. test OrderResource
-    // 3.1 GET /
+    // 3. test SellerResource
+    // 3.1 GET /seller: return seller profile
+    @Test
+    public void testSeller() {
+        String url = baseUrl + "seller";
+        Response response = given().
+                header("Authorization", sellerToken).
+                when().
+                get(url).
+                then().
+                statusCode(200).
+                extract().response();
+        assertEquals(response.jsonPath().getString("email"), sellerEmail);
+        assertEquals(response.jsonPath().getString("type"), "Seller");
+    }
+
+    // 3.2 GET /sellers: return all sellers
+    @Test
+    public void testSellers() {
+        String url = baseUrl + "sellers";
+        Response response = given().
+                header("Authorization", sellerToken).
+                when().
+                get(url).
+                then().
+                statusCode(200).
+                extract().response();
+        assertTrue(response.jsonPath().getList("email").size() > 0);
+        assertTrue(response.jsonPath().getList("numberOfSells").size() > 0);
+    }
+
+    // 3.3 GET /sellers/ranking: return a list of sellers who sells the most
+    @Test
+    public void testSellerRanking() {
+        String url = baseUrl + "sellers/ranking";
+        Response response = given().
+                header("Authorization", sellerToken).
+                when().
+                get(url).
+                then().
+                statusCode(200).
+                extract().response();
+        List<Integer> sellsOrBuys = response.jsonPath().getList("sellsOrBuys");
+        assertTrue(Utils.isInOrder(sellsOrBuys, false));
+    }
+
+    // 3.4 POST /create/seller: create a new seller
+    @Test
+    public void testCreateSeller() {
+        String url = baseUrl + "create/seller";
+        String body = "{" +
+                "\"name\": \"testCreateSeller1\", " +
+                "\"email\": \"testCreateSeller1@gmail.com\", " +
+                "\"password\": \"testCreateSeller1\" " +
+                "}";
+        Response resp = given().
+                contentType("application/json").
+                body(body).
+                when().
+                post(url).
+                then().
+                extract().response();
+        String response = login(
+                "testCreateSeller1@gmail.com",
+                "testCreateSeller1"
+        );
+        assertTrue(response.startsWith("Bearer "));
+    }
+
+    // 3.4.2 create a new seller with an existing email
+    @Test
+    public void testCreateSellerWithExistingEmail() {
+        String url = baseUrl + "create/seller";
+        String body = "{" +
+                "\"name\": \"testCreateSeller1\", " +
+                "\"email\": \"testCreateSeller1@gmail.com\", " +
+                "\"password\": \"testCreateSeller1\" " +
+                "}";
+        Response resp = given().
+                contentType("application/json").
+                body(body).
+                when().
+                post(url).
+                then().
+                extract().response();
+        assertEquals(resp.statusCode(), 400);
+        assertTrue(resp.jsonPath().getString("message").
+                contains("This email is already being used"));
+    }
+
+    // 3.5 UPDATE /update/seller: update seller profile
+    @Test
+    public void testUpdateSeller() throws JSONException {
+        String url = baseUrl + "update/seller";
+        String currToken = login(
+                "testCreateSeller1@gmail.com",
+                "testCreateSeller1"
+        );
+        String newPassword = "testUpdate";
+        JSONObject json = new JSONObject();
+        json.put("name", "testCreateSeller1");
+        json.put("email", "testCreateSeller1@gmail.com");
+        json.put("password", newPassword);
+        Response resp = given().
+                header("Authorization", currToken).
+                contentType("application/json").
+                body(json.toString()).
+                when().
+                put(url).
+                then().
+                extract().response();
+        String response = login(
+                "testCreateSeller1@gmail.com",
+                newPassword
+        );
+        assertTrue(response.startsWith("Bearer "));
+
+        // modify back to the original password
+        json.put("password", "testCreateSeller1");
+        given().
+                header("Authorization", currToken).
+                contentType("application/json").
+                body(json.toString()).
+                when().
+                put(url).
+                then().
+                statusCode(200);
+    }
+
+    // 3.6 DELETE /delete/seller: delete a seller
+    @Test
+    public void testDeleteSeller() {
+        String url = baseUrl + "delete/seller";
+        String currToken = login(
+                "testCreateSeller1@gmail.com",
+                "testCreateSeller1"
+        );
+        Response resp = given().
+                header("Authorization", currToken).
+                when().
+                delete(url).
+                then().
+                statusCode(204).
+                extract().response();
+
+        // try to log in with the deleted account
+        login("testCreateSeller1@gmail.com", "testCreateSeller1", 401);
+    }
+
 }
